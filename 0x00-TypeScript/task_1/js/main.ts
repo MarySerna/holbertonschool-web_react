38863077bf9345c6095a2a8ad56ef053fa8043e6
Mary Serna <mary.serna.8903@gmail.com>
2021-02-09 22:42:01 -0500

    ts

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -1,65 +1,38 @@
-// Teacher interface
 interface Teacher {
   readonly firstName: string;
   readonly lastName: string;
   fullTimeEmployee: boolean;
-  location: string;
   yearsOfExperience?: number;
+  location: string;
   [propName: string]: any;
 }
 
-// Directors interface
 interface Directors extends Teacher {
   numberOfReports: number;
 }
 
-// Creating a teacher and a director
-const teacher3: Teacher = {
-  firstName: 'John',
-  fullTimeEmployee: false,
-  lastName: 'Doe',
-  location: 'London',
-  contract: false,
-};
-console.log(teacher3);
-
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
-console.log(director1);
 
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
-const printTeacher: printTeacherFunction = function (
+export const printTeacher: printTeacherFunction = function (
   firstName: string,
   lastName: string
 ): string {
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-//console.log(printTeacher('John', 'Doe'));
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
 
-// class description interface
-interface classInterface {
-  firstName: string;
-  lastName: string;
+interface StudentClassInterface {
   workOnHomework(): string;
   displayName(): string;
 }
 
-// constructor description interface
-interface classConstructor {
-  new (firstName: string, lastName: string): classInterface;
-}
-
-// Creating class and constructor through interfaces 
-class StudentClass implements classInterface {
+export class StudentClass implements StudentClassInterface {
   firstName: string;
   lastName: string;
 
@@ -69,15 +42,10 @@ class StudentClass implements classInterface {
   }
 
   workOnHomework(): string {
-    return 'Currently working';
+    return "Currently working";
   }
 
   displayName(): string {
     return this.firstName;
   }
 }
-
-// creating instance of StudentClass
-const studentClass: StudentClass = new StudentClass('Mary', 'Serna');
-console.log(studentClass.displayName());
-console.log(studentClass.workOnHomework());
