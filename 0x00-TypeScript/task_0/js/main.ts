5b6417f80d86c9dbc74e94d4223b6da90ceceb6c
Mary Serna <mary.serna.8903@gmail.com>
2021-02-09 22:09:48 -0500

    ts

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -4,14 +4,12 @@ interface Student {
   age: number;
   location: string;
 }
-
 const firstSudent: Student = {
   firstName: 'Shakira',
   lastName: 'Doe',
   age: 18,
   location: 'Miami',
 };
-
 const secondSudent: Student = {
   firstName: 'Juan',
   lastName: 'Doe',
@@ -21,20 +19,16 @@ const secondSudent: Student = {
 
 const studentsList: Array<Student> = [ firstSudent, secondSudent ];
 
-const body: HTMLBodyElement = document.getElementsByTagName('body')[0];
+const table = document.createElement('table') as HTMLTableElement;
 
-const table: HTMLTableElement = document.createElement('table');
-const thead: HTMLTableSectionElement = document.createElement('thead');
-const tbody: HTMLTableSectionElement = document.createElement('tbody');
-
-studentsList.forEach((student) => {
-  const row: HTMLTableRowElement = tbody.insertRow(0);
-  const cell1: HTMLTableCellElement = row.insertCell(0);
-  const cell2: HTMLTableCellElement = row.insertCell(1);
-
-  cell1.innerHTML = student.firstName;
-  cell2.innerHTML = student.location;
+studentsList.forEach((student: Student) => {
+  const row = table.insertRow()
+  const name = document.createElement('td');
+  const location = document.createElement('td');
+  name.textContent = student.firstName;
+  location.textContent = student.location;
+  row.appendChild(name);
+  row.appendChild(location);
 });
 
-table.append(tbody);
-body.append(table);
+document.body.appendChild(table);
