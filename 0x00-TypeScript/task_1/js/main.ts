661d9fe00e320b81f952009bf1390bce72e21b26
Mary Serna <mary.serna.8903@gmail.com>
2021-02-09 22:53:15 -0500

    ts

--- 0x00-TypeScript/task_1/js/main.ts
+++ 0x00-TypeScript/task_1/js/main.ts
@@ -4,17 +4,40 @@ interface Teacher {
   fullTimeEmployee: boolean;
   yearsOfExperience?: number;
   location: string;
+  /* eslint-disable @typescript-eslint/no-explicit-any */
   [propName: string]: any;
+  /* eslint-enable @typescript-eslint/no-explicit-any */
 }
+
+// const teacher3: Teacher = {
+//   firstName: "John",
+//   fullTimeEmployee: false,
+//   lastName: "Doe",
+//   location: "London",
+//   contract: false,
+// };
+
+// console.log(teacher3);
 
 interface Directors extends Teacher {
   numberOfReports: number;
 }
- 
+
+// const director1: Directors = {
+//   firstName: "John",
+//   lastName: "Doe",
+//   location: "London",
+//   fullTimeEmployee: true,
+//   numberOfReports: 17,
+// };
+// console.log(director1);
+
+/* eslint-disable @typescript-eslint/class-name-casing */
 interface printTeacherFunction {
   (firstName: string, lastName: string): string;
 }
 
+/* eslint-enable @typescript-eslint/class-name-casing */
 export const printTeacher: printTeacherFunction = function (
   firstName: string,
   lastName: string
@@ -22,13 +45,15 @@ export const printTeacher: printTeacherFunction = function (
   return `${firstName.charAt(0)}. ${lastName}`;
 };
 
-interface StudentClassInterface {
-  workOnHomework(): string;
-  displayName(): string;
-}
+// console.log(printTeacher("John", "Doe"));
 
 interface StudentConstructor {
   new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
 }
 
 export class StudentClass implements StudentClassInterface {
