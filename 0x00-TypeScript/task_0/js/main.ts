e4856ebfea19368982dfe854bd4c15e36a17bd5f
Mary Serna <mary.serna.8903@gmail.com>
2021-02-09 22:11:10 -0500

    ts

--- 0x00-TypeScript/task_0/js/main.ts
+++ 0x00-TypeScript/task_0/js/main.ts
@@ -22,9 +22,9 @@ const studentsList: Array<Student> = [ firstSudent, secondSudent ];
 const table = document.createElement('table') as HTMLTableElement;
 
 studentsList.forEach((student: Student) => {
-  const row = table.insertRow()
-  const name = document.createElement('td');
-  const location = document.createElement('td');
+  const row: HTMLTableRowElement = table.insertRow()
+  const name: HTMLTableCellElement = document.createElement('td');
+  const location: HTMLTableCellElement = document.createElement('td');
   name.textContent = student.firstName;
   location.textContent = student.location;
   row.appendChild(name);
