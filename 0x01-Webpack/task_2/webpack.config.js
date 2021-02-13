8699562ee42856a601188e3b948f0f70cc5ddb6e
Mary Serna <mary.serna.8903@gmail.com>
2021-02-12 19:20:40 -0500

    wp

--- 0x01-Webpack/task_2/webpack.config.js
+++ 0x01-Webpack/task_2/webpack.config.js
@@ -1,35 +1,38 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    mode: 'production',
-    entry : path.resolve(__dirname, 'js/dashboard_main.js'),
-    output: {
-        path: path.resolve(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    performance: {
-      maxAssetSize: 1000000
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          use: [
-              'style-loader',
-              'css-loader'
-            ]
-        },
-        {
-          test: /\.jpg$/,
-          use: {
-            loader: 'file-loader'
-          }
-        }
-      ]
-    },
-    optimization: {
-      splitChunks: {
-        chunks: 'all'
-      }
-    }
-}
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }, 
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          'file-loader',
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ],
+      },
+    ]
+  }
+};
