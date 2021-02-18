939512d4cafbf6810655168a14a77e635c8aecf2
Mary Serna <mary.serna.8903@gmail.com>
2021-02-17 23:37:55 -0500

    task_4

--- 0x02-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x02-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,5 +1,4 @@
-import React from 'react';
+import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import { shallow, configure } from 'enzyme';
 
-configure({adapter: new Adapter()});
+Enzyme.configure({ adapter: new Adapter() });
